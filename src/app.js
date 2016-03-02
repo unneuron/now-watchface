d77122398ce3d840b362bfbd3f094bdce93fe3f5
David Emanuel Oprea <unneuron@users.noreply.github.com>
2016-03-03 01:59:10 +0200

    1.3 Release, fix custom text
    Now the custom text is loaded from Settings.data

--- src/app.js
+++ src/app.js
@@ -10,23 +10,6 @@ var Accel = require('ui/accel');
 var Settings = require('settings');
 var NowText = defaultText;
 
-
-// Set a configurable with the open callback
-Settings.config(
-  { url: 'http://dev.unneuron.ro/now-watch.html' },
-  function(e) {
-    console.log('opening configurable');
-
-    Settings.option('now', NowText);
-  },
-  function(e) {
-    NowText = e.options.now;
-    now.text(NowText);
-    console.log('closed configurable');
-  }
-);
-
-
 var now = new UI.Text({
   position: new Vector2(0, 60),
   size: new Vector2(144, 168),
@@ -69,9 +52,24 @@ var main = new UI.Window({
 });
 
 
+// Set a configurable with the open callback
+Settings.config(
+  { url: 'http://dev.unneuron.ro/now-watch.html', autoSave: true },
+  function(e) {
+    console.log('opening configurable');
+    Settings.data('now', NowText);
+  },
+  function(e) {
+    NowText = e.options.now;
+    Settings.data('now', NowText);
+    now.text(NowText);
+    console.log('closed configurable');
+  }
+);
+
 Accel.config({
   rate: 10,
-  samples: 10,
+  samples: 25,
   subscribe: true
 });
 
@@ -79,19 +77,29 @@ Accel.on('tap', function(e) {
   var curDate = new Date();
   var hour = curDate.getHours() > 12 ? ( curDate.getHours()-12 ) : curDate.getHours();
   var minute = curDate.getMinutes();
-  var ampm = curDate.getHours() > 12 ? "PM" : "AM";
+  var ampm = curDate.getHours() > 12 ? "pm" : "am";
   if (hour.toString().length == 1) {
     hour = "0" + hour.toString();
   }
   if (minute.toString().length == 1) {
     minute = "0" + minute.toString();
   }
-  
-  now.text(hour + " : " + minute + " " + ampm);
+  var displayText = hour + " : " + minute + " " + ampm;
+ 
+  now.text(displayText);
   setTimeout(function(){
     now.text(NowText);
-  }, 3000);
+  }, 4000);
 });
+
+var prefs = Settings.data('now');
+if (prefs) {
+  NowText = prefs;
+  now.text(NowText);
+} else {
+  Settings.data('now', NowText);
+}
+
 
 main.add(emptyTopSpace);
 main.add(now);
